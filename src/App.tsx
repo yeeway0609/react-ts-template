ca62d1933cb68567da67382a68120c45e3f1f0e8
yeeway <yiwei.suuu@gmail.com>
2025-07-28 16:27:14 +0800

    refactor: remove unused eslint-plugin-tailwindcss from dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold m-[1.25rem] underline p-2 p-3 text-red-400">Hello world!</h1>
+      <h1 className="text-3xl font-bold m-[1.25rem] underline text-red-400">Hello world!</h1>
     </>
   )
 }
-
