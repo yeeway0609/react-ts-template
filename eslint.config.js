dc95b0dfeb666014846621a471690f2d301b5692
yeeway <yiwei.suuu@gmail.com>
2025-07-28 15:51:24 +0800

    style: update heading styles in App component for improved spacing and padding

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,38 @@
-import globals from 'globals'
-import js from '@eslint/js'
-import ts from 'typescript-eslint';
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { globalIgnores } from 'eslint/config'
+import globals from "globals"
+import js from "@eslint/js"
+import react from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import ts from "typescript-eslint"
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default [
+  js.configs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat["jsx-runtime"],
+  reactHooks.configs["recommended-latest"],
+  reactRefresh.configs.vite,
+  ...ts.configs.recommended,
   {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    extends: [
-      js.configs.recommended,
-      ts.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    ignores: ["dist", "node_modules"],
   },
-])
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+]
